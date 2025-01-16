ecd766161b6d782671b6175f9bceba2731b09a54
KPorus <designerfardin2021@gmail.com>
2025-01-16 19:56:13 +0600

    add a utils function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import DesktopPage from "@/components/desktop";
+import { isMobile } from "@/utils/isMobile";
+import { headers } from "next/headers";
 
 export default function Home() {
+  const userAgent = headers().get("user-agent") || "";
+	const isMobileDevice = isMobile(userAgent);
   return (
     <div className="container">
-      <DesktopPage />
+      {isMobileDevice ? (
+        <div className="flex justify-center items-center bg-neutral-950 text-white font-bold h-screen p-4 text-center">Mobile view is not developed. Please go back to Desktop view or Desktop.</div>
+      ) : (
+          <DesktopPage />
+				)}
     </div>
   );
 }
